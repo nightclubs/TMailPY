797a06194f1c8b0a99cc3e48d22c14e0027763ae
blog <116030602+accusable@users.noreply.github.com>
2022-10-23 12:01:51 -0400

    Update example.py

--- example.py
+++ example.py
@@ -5,17 +5,14 @@ mail = Mail
 def get_email():
  print('generated email >> {}'.format(mail.create_email()))
 
-get_email()
 
 def get_emails():
  print('mail >> {}'.format(mail.get_mail('test@lasagna.email')))
 
-# get_emails()
 
 def check_mail():
  print('content >> {}'.format(mail.check_mail('mail id'))) # the mail ID is the numbers in the link, you will see mail id when you use the get_emails function.
 
-# check_mail()
 
 # ----------------------------------- This top part uses lasagna.email domain only, the bottom part has 2 - 3 different domains!
 
