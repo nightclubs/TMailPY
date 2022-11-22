4ff1e9c565668bcac9d3789cf115993ae00edceb
zt <116030602+nightclubs@users.noreply.github.com>
2022-11-22 08:11:28 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,39 +1,20 @@
 from wrapper import *
 
-mail = Mail
+mail = TMailPY
 
+def new_email():
+    email = mail.create_email()
+    return email
 
-def get_email():
-    print("generated email >> {}".format(mail.create_email()))
-
-
-def get_emails():
-    print("mail >> {}".format(mail.get_mail("test@lasagna.email")))
-
-
-def check_mail():
-    print(
-        "content >> {}".format(mail.check_mail("mail id"))
-    )  # the mail ID is the numbers in the link, you will see mail id when you use the get_emails function.
-
-
-# ----------------------------------- This top part uses lasagna.email domain only, the bottom part has 2 - 3 different domains!
-
-
-email = Email
-
-
-def get_email():
-    print(
-        "generated email >> {}".format(email.create_email("domain here"))
-    )  # the domains are @lasagna.pro, @rblx.rocks and @linustechtips.email
-
-
-def get_emails():
-    print(
-        "mail >> {}".format(email.get_mail("test@lasagna.pro"))
-    )  # you could also choose from the other domains listed above!
-
+def get_mail():
+    emails = mail.get_mail(new_email())
+    return emails
 
 def check_mail():
-    print("content >> {}".format(email.check_mail("mail id")))
+    email_ids = mail.check_mail(get_mail())
+    return email_ids
+
+
+
+
+
